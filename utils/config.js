4d5330abce90e3cfec0563c0cf48ce4110c8d5e8
Daria Khanina <khaninadaria@gmail.com>
2023-11-05 22:39:22 +0300

    fix: changed host

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const { NODE_ENV, JWT_SECRET, MONGODB_URI } = process.env;
 
 const SECRET_KEY = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
-const MONGO_URL = MONGODB_URI || 'mongodb://127.0.0.1:27017/bitfilmsdb';
+const MONGO_URL = MONGODB_URI || 'mongodb://localhost:27017/bitfilmsdb';
 
 module.exports = {
   SECRET_KEY,
