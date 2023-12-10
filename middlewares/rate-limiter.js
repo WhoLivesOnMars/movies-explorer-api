6a51335129ea6051073a8d31d0b081e6d8d81357
Daria Khanina <khaninadaria@gmail.com>
2023-12-10 20:54:54 +0300

    fix: changed rate limiter

--- middlewares/rate-limiter.js
+++ middlewares/rate-limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 10000000000,
   message: 'В настоящий момент превышено количество запросов на сервер. Пожалуйста, попробуйте повторить позже',
 });
 
