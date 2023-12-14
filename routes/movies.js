ffd96d171d2438c2ff0dca0257cd146eb8dc5afe
Daria Khanina <khaninadaria@gmail.com>
2023-12-14 16:55:39 +0300

    fix: changed logic

--- routes/movies.js
+++ routes/movies.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const { createMovieValidation, deleteMovieValidation } = require('../middlewares/validation');
 const {
-  getMovies, getUserMovies, createMovie, deleteMovie,
+  getMovies, createMovie, deleteMovie,
 } = require('../controllers/movies');
 
-router.get('/all', getMovies);
-router.get('/', getUserMovies);
+router.get('/', getMovies);
+// router.get('/', getUserMovies);
 router.post('/', createMovieValidation, createMovie);
-router.delete('/:_id', deleteMovieValidation, deleteMovie);
+router.delete('/:movieId', deleteMovieValidation, deleteMovie);
 
 module.exports = router;
